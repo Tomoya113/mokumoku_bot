e99e505fea0d7eeb71f830d1958e68576c04d81a
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-03-27 00:19:39 +0900

    進捗

--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,64 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { WebClient } from '@slack/web-api'
 import dotenv from 'dotenv'
+import firebase from 'firebase'
+import 'firebase/firestore'
 
 dotenv.config()
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 // NOTE: 何か良い書き方ないかな
 const token = process.env.token
+const apiKey = process.env.apiKey
+const authDomain = process.env.authDomain
+const projectId = process.env.projectId
 const client  = new WebClient(token)
 const channel =  "C01MHAGJE4F"
 
-// 投稿のIDを保存するやつ
-let ts: any = ""
-let users: string[] = []
-let count = 0;
+firebase.initializeApp({
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+})
+
+const db = firebase.firestore()
+const roomRef = db.collection("room").doc("information")
+const usersRef = db.collection("users")
 
 app.get('/', (req, res) => {
-  res.send("ok")
+  const array: string[] = []
+  usersRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      array.push(doc.id)
+    })
+  })
+  const text = array.join(', ')
+  const count = array.length
+  return res.send(`${text}, ${count}`)
 })
 
 app.post('/zoom/webhook', (req, res) => {
   const user = req.body.payload.object.participant.user_name
   const event = req.body.event
   if(event == "meeting.participant_joined") {
-    count += 1
-    users.push(user)
-    console.log("a user joined the meeting", `currentUser: ${users}`);
-    if(ts !== "") {
-      deleteRoomStatusMessage()
-    }
+    // ここを書き換える
+    usersRef.doc(user).set({
+      name: user
+    })
+    .then(() => {
+      console.log("document set");
+    })
+    deleteRoomStatusMessage()
     postRoomStatusMessage()
     res.send("ok")
   } else if(event == "meeting.participant_left") {
-    count -= 1
-    if(count < 0) count = 0;
-    users = users.filter(u => {
-      return u !== user
-    })
-    console.log("a user left the meeting.", `currentUser: ${users}`);
+    usersRef.doc(user).delete().then(() => {
+      console.log("Document successfully deleted!");
+    }).catch((error) => {
+      console.error("Error removing document: ", error);
+    });
     deleteRoomStatusMessage()
     postRoomStatusMessage()
     res.send("ok")
@@ -51,25 +71,40 @@ app.post('/zoom/webhook', (req, res) => {
 app.listen(process.env.PORT || 3000, () => console.log("server is listening"))
 
 const generateRoomStatusText = () => {
-  const text = `
-  *もくもく会の状況*\n現在の参加人数: ${count}\n現在のメンバー: ${generateRoomMemberText()}
-  `
-  return text
+  let { text, count } = generateRoomMemberText()
+  const result = `
+  *もくもく会の状況*\n現在の参加人数: ${count}\n現在のメンバー: ${text}`
+  return result
 }
 
 const generateRoomMemberText = () => {
-  const text = users.join(', ')
-  return text
+  const array: string[] = []
+  usersRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      array.push(doc.id)
+    })
+  })
+  const text = array.join(', ')
+  const count = array.length
+  return {text, count}
 }
 
 const deleteRoomStatusMessage = () => {
-  client.chat.delete({
-    channel: channel,
-    ts: ts
-  }).then(() => {
-    console.log("deleted message");
-  }).catch( error => {
-    console.log(error);
+  roomRef.get().then((doc) => {
+    if (doc.exists) {
+      client.chat.delete({
+        channel: channel,
+        ts: doc.data().id
+      }).then(() => {
+        console.log("deleted message");
+      }).catch( error => {
+        console.log(error);
+      })
+    } else {
+      console.log("No such document!");
+    }
+  }).catch((error) => {
+    console.log("Error getting document:", error);
   })
 }
 
@@ -78,8 +113,17 @@ const postRoomStatusMessage = () => {
     channel: channel,
     text: generateRoomStatusText()
   }).then(response => {
-    ts = response.ts
+    saveTextId(response.ts)
   }).catch( error => {
     console.log(error);
+  })
+}
+
+const saveTextId = (postId: any) => {
+  roomRef.set({
+    id: postId
+  })
+  .then(() => {
+    console.log("document set");
   })
 }
